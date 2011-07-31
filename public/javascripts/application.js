1d61c8aee127bd1cc1bb4621117912c3ee569292
Ben Darlow <github@kapowaz.net>
2011-07-31 15:17:12 +0100

    Added console output to JS polling

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,20 +1,29 @@
 $(document).ready(function() {
   
   jQuery.fn.requestedPage = function requestedPage(){
-    var page    = $(this);
-    var url     = page.attr('data-page-url');
-    var page_id = page.attr('data-page-id');
-    var timer   = null;
-    var pending = true;
+    var page       = $(this);
+    var url        = page.attr('data-page-url');
+    var page_id    = page.attr('data-page-id');
+    var timer      = null;
+    var pending    = true;
+    var poll_count = 0;
+    
+    if (console && console.log) console.log('Polling status of requested page: ' + url);
     
     var pollStatus = function pollStatus(){
       if (pending) {
+        poll_count++;
+        
+        if (console && console.log) console.log('Poll count: ' + poll_count);
+        
         jQuery.ajax({
           url: '/pages/' + page_id + '.json',
           success: function(response, textStatus, jqXHR){
             if (response.pending) {
+              if (console && console.log) console.log('Fetch is still pending.');
               timer = setTimeout(pollStatus, 5000);
             } else {
+              if (console && console.log) console.log('Fetch complete after ' + 5 * poll_count + ' seconds.');
               pending = false;
               clearTimeout(timer);
               page.find('span.status').removeClass('pending').text('and this is what it contained:');
